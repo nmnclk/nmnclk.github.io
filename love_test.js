a644b5e882e801bc9ec7891914bc431ed392bba9
Numan Çalık <numancalik42@gmail.com>
2022-12-12 00:21:38 +0300

    n s

--- love_test.js
+++ love_test.js
@@ -6,67 +6,63 @@ function hesapla() {
     name1 = name1.toLowerCase();
     name2 = name2.toLowerCase();
 
-    if (name1 == "şeyma" && name2 == atob("bnVtYW4=")) {
 
-        document.getElementById("result").innerHTML = "<br>Sonuç : <h1>&#128151; %" + 10000000000 + "&#128151;</h1>";
-        document.getElementById("sonuc").innerHTML = " <h1>&#128151;</h1>";
-    } else {
-        nameArr = (name1 + name2).split("");
-        nameArr2 = (name1 + name2).split("");
-        var countArr = [];
-        text = "";
-        for (let i = 0; i < nameArr.length; i++) {
-            count = 0;
-            text += nameArr[i] + "<br>"
+    nameArr = (name1 + name2).split("");
+    nameArr2 = (name1 + name2).split("");
+    var countArr = [];
+    text = "";
+    for (let i = 0; i < nameArr.length; i++) {
+        count = 0;
+        text += nameArr[i] + "<br>"
 
-            for (let j = i; j < nameArr2.length; j++) {
-                if (nameArr[i] == nameArr2[j]) {
-                    count++;
-                    nameArr2[j] = "/";
-                    text += nameArr2 + "<br>";
-                }
-            }
-            text += "--------------------- <br>"
-            if (count != 0) {
-                countArr.push(count);
+        for (let j = i; j < nameArr2.length; j++) {
+            if (nameArr[i] == nameArr2[j]) {
+                count++;
+                nameArr2[j] = "/";
+                text += nameArr2 + "<br>";
             }
         }
-
-        text += "<p>" + countArr;
-        result = 0;
-        arr2 = [];
-        text += "<br>--------------------- <br>"
-        while (countArr.length > 2) {
-
-            while (countArr.length > 0) {
-                if (countArr.length == 1) {
-                    arr2.push(countArr[0]);
-                    break;
-                }
-                text += "<br> önceki liste: " + countArr;
-                arr2.push(countArr[0] + countArr[countArr.length - 1]);
-                countArr.shift();
-                countArr.pop(countArr.length - 1);
-
-                text += "<br>  listede kalanlar: " + countArr;
-                text += "<br> oluşan liste : " + arr2;
-                text += "<br>---------------------"
-            }
-            if (arr2.length > 2) {
-                text += "<br> Oluşturulan listeye işlem baştan uygulanıyor ";
-                text += "<br>---------------------"
-            }
-
-            countArr = arr2;
-            arr2 = [];
+        text += "--------------------- <br>"
+        if (count != 0) {
+            countArr.push(count);
         }
-        text += "<br>Sonuç : <h1>&#128151;" + countArr[0] + "" + countArr[1] + "&#128151;</h1>";
-        document.getElementById("result").innerHTML = "<br>Sonuç : <h1>&#128151; %" + countArr[0] + "" + countArr[1] + "&#128151;</h1>";
-        document.getElementById("sonuc").innerHTML = text;
-
-
     }
+
+    text += "<p>" + countArr;
+    result = 0;
+    arr2 = [];
+    text += "<br>--------------------- <br>"
+    while (countArr.length > 2) {
+
+        while (countArr.length > 0) {
+            if (countArr.length == 1) {
+                arr2.push(countArr[0]);
+                break;
+            }
+            text += "<br> önceki liste: " + countArr;
+            arr2.push(countArr[0] + countArr[countArr.length - 1]);
+            countArr.shift();
+            countArr.pop(countArr.length - 1);
+
+            text += "<br>  listede kalanlar: " + countArr;
+            text += "<br> oluşan liste : " + arr2;
+            text += "<br>---------------------"
+        }
+        if (arr2.length > 2) {
+            text += "<br> Oluşturulan listeye işlem baştan uygulanıyor ";
+            text += "<br>---------------------"
+        }
+
+        countArr = arr2;
+        arr2 = [];
+    }
+    text += "<br>Sonuç : <h1>&#128151;" + countArr[0] + "" + countArr[1] + "&#128151;</h1>";
+    document.getElementById("result").innerHTML = "<br>Sonuç : <h1>&#128151; %" + countArr[0] + "" + countArr[1] + "&#128151;</h1>";
+    document.getElementById("sonuc").innerHTML = text;
+
+
 }
+
 $("#sonuc").hide();
 function showHide() {
     if ($("#showHideC").is(":checked")) {
